3d8e29b73183c234e5422e51d730b7ee13facb5b
toxie <gamepiatnitza@gmail.com>
2025-10-03 21:06:31 +0300

    readability update for CSVToJSON

--- modules/buses/getBusStopInfo.js
+++ modules/buses/getBusStopInfo.js
@@ -9,29 +9,28 @@ function getMinutesUntilBus(fetched_time) {
     return minutes_until_bus;
 }
 
-const HEADERS = ["bus_type", "bus_num", "bus_direction", "bus_time"]
-const INDICES = [0, 1, 5, 3]
+function getField(n) {
+    return line => line[n]
+}
+
+const busMap = {
+    bus_type: getField(0),
+    bus_num: getField(1),
+    bus_direction: getField(5),
+    bus_time: line => `${getMinutesUntilBus(line[3])} min`
+}
+
+function parseBus(line) {    
+    line = line.split(",");
+    const bus = Object.fromEntries(Object.entries(busMap).map(([header, parseValue]) => [header, parseValue(line)]))
+
+    return bus;
+}
 
 function CSVToJSON(csv) {
 
-    let bus_lines = csv.trim().split("\n");
-    let json = [];
-
-    for(let i = 1; i < bus_lines.length; i++){
-
-	    let bus = {};
-	    let current_line = bus_lines[i].split(",");
-
-	    for(let j = 0; j < HEADERS.length - 1; j++){
-		    bus[HEADERS[j]] = current_line[INDICES[j]];
-	    }
-
-        bus[HEADERS.at(-1)] = `${getMinutesUntilBus(current_line[INDICES.at(-1)])} min`;
-
-	    json.push(bus);
-    }
-
-    return json;
+    let bus_lines = csv.trim().split("\n").slice(1,);
+    return bus_lines.map(parseBus);
 }
 
 /**
@@ -57,3 +56,5 @@ async function getBusStopInfo(stop_id) {
     const response = await fetch(url);
     return CSVToJSON(await response.text());
 }
+
+console.log(await getBusStopInfo(2016))
