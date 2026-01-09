2102c23ae072fc6060f8dee72bc7666eee27485c
toxie <gamepiatnitza@gmail.com>
2026-01-09 20:18:40 +0200

    fix buses' display and make use of the module's ability to gather timetables for multiple bus stops at once Fixes #16

--- html/buses/script.js
+++ html/buses/script.js
@@ -1,50 +1,47 @@
 const mainMain = document.querySelector("main");
 
-const stops = [
-    {id: "2016", name: "Licėjus", dir: "Akropolio kryptimi"},
-    {id: "0710", name: "Licėjus", dir: "Žirmunų kryptimi"},
-    {id: "0804", name: "Pramogų arena", dir: "Žirmūnų kryptimi"},
-    {id: "0802", name: "Pramogų arena", dir: "Santariškių kryptimi"},
-    {id: "0709", name: "Pramogų arena", dir: "Centro kryptimi"},
-    {id: "2015", name: "Pramogų arena", dir: "Ozo kryptimi"},
-    {id: "0708", name: "Tauragnų st.", dir: "Centro kryptimi"},
-    {id: "0803", name: "Tauragnų st.", dir: "Santariškių kryptimi"}
-];
+const stops = {
+    "2016": {name: "Licėjus", dir: "Akropolio kryptimi"},
+    "0710": {name: "Licėjus", dir: "Žirmunų kryptimi"},
+    "0804": {name: "Pramogų arena", dir: "Žirmūnų kryptimi"},
+    "0802": {name: "Pramogų arena", dir: "Santariškių kryptimi"},
+    "0709": {name: "Pramogų arena", dir: "Centro kryptimi"},
+    "2015": {name: "Pramogų arena", dir: "Ozo kryptimi"},
+    "0708": {name: "Tauragnų st.", dir: "Centro kryptimi"},
+    "0803": {name: "Tauragnų st.", dir: "Santariškių kryptimi"}
+};
 function generateTables() {
-    stops.forEach(stop => {
+    Object.keys(stops).forEach(stopId => {
         const table = document.createElement("div");
         const header = document.createElement("div");
-        header.innerHTML = stop.name + "<br>" + stop.dir;
+        header.innerHTML = stops[stopId].name + "<br>" + stops[stopId].dir;
         header.classList.add("bus_header");
         table.appendChild(header);
         const body = document.createElement("div");
-        body.id = "table_" + stop.id;
+        body.id = "table_" + stopId;
         body.classList.add("bus_list");
         table.appendChild(body);
         mainMain.appendChild(table);
     });
 }
-async function loadBuses(stopId){
-    const response = await fetch(`/api/buses/${stopId}`).then(res => res.json());
-    const table = document.getElementById("table_" + stopId);
-    table.innerHTML = "";
-    // let counter = 0;
-    response.forEach(bus => {
-        // if(counter >= 10) return;
-        // counter++;
+async function updateStops(stopIds){
+    let response = await fetch(`/api/buses?stop_ids=${stopIds}`);
+    response = await response.json();
+    Object.keys(response).forEach(stopId => {
+        const table = document.getElementById("table_" + stopId);
+        table.innerHTML = "";
+        response[stopId].forEach(bus => {
 
-        const busDiv = document.createElement("div");
-        busDiv.innerHTML = `<span class="bus_num type_${bus.bus_type}">${bus.bus_num}</span> ${bus.bus_time}`;
-        table.appendChild(busDiv);
-    });
-}
-async function updateStops(){
-    stops.forEach(stop => loadBuses(stop.id));
+            const busDiv = document.createElement("div");
+            busDiv.innerHTML = `<span class="bus_num type_${bus.bus_type}">${bus.bus_num}</span> ${bus.bus_time}`;
+            table.appendChild(busDiv);
+        });
+    })
 }
 
 function main() {
     generateTables();
-    updateStops();
+    updateStops(Object.keys(stops));
     setInterval(updateStops, 1000*5);
 }
 
