d9545842070a152c960e0f142b08782e7c1648bc
Rokas159 <rokas.r.rimkus@gmail.com>
2026-04-24 15:32:42 +0300

    Change time to Lithuanian locale

--- html/header.js
+++ html/header.js
@@ -1,6 +1,6 @@
 function updateTime() {
     const now = new Date();
-    document.getElementById("time").textContent = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    document.getElementById("time").textContent = now.toLocaleTimeString("lt-LT", { hour: '2-digit', minute: '2-digit' });
 }
 
 updateTime();
